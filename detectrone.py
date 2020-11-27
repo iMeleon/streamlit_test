4b4ac4819e39f8669991ff0839141ff5ae1954ac
iMeleon <scentofdarkness@gmail.com>
2020-11-27 19:32:49 +0300

    add detectrone.py

--- detectrone.py
+++ detectrone.py
@@ -11,7 +11,7 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_F
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 # set threshold for this model
 cfg.MODEL.WEIGHTS =model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
 # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
-cfg.MODEL.DEVICE='cpu'
+# cfg.MODEL.DEVICE='cpu'
 print('228')
 predictor = DefaultPredictor(cfg)
 print('loh2')
