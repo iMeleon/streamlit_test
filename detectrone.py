223dcf827fb446d26c96ade8fe6ecf0a8532dcbc
iMeleon <scentofdarkness@gmail.com>
2020-11-27 21:59:00 +0300

    dswa

--- detectrone.py
+++ detectrone.py
@@ -10,8 +10,8 @@ setup_logger()
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 # set threshold for this model
-# cfg.MODEL.WEIGHTS =model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 # set threshold for this model
+cfg.MODEL.WEIGHTS ="model_final_280758.pkl"
 # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
 cfg.MODEL.DEVICE = "cpu"
 print('2285ggit')
